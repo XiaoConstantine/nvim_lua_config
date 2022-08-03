fe4fcb865c149c02a4807f33b559acac957b0690
Xiao Cui <constantine124@gmail.com>
2022-08-03 12:02:25 -0400

    Auto create buffer split

--- lua/auto-run/lua.lua
+++ lua/auto-run/lua.lua
@@ -19,10 +19,47 @@ local attach_to_buffer = function(output_bufnr, pattern, command)
     })
 end
 
+
+local create_split_buf = function(width)
+    local bufnr = vim.api.nvim_create_buf(true, true)
+    vim.cmd(("belowright vertical sbuffer " .. bufnr))
+    local winnr = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_width(winnr, width)
+    return bufnr
+end
+
+
+local default_cmd = function(filetype, mode)
+    local cmd = ""
+    if filetype == "python" then
+        cmd = "python3"
+        if mode == "test" then
+            cmd = cmd .. " pytest"
+        end
+    elseif filetype == "go" then
+        cmd = "go"
+        if mode == "test" then
+            cmd = cmd .. " test"
+        end
+    elseif filetype == "zig" then
+        cmd = "zig"
+        if mode == "test" then
+            cmd = cmd .. " test"
+        end
+    end
+
+    print(cmd)
+    return cmd
+end
+
+
 vim.api.nvim_create_user_command("AutoRun", function()
     print "AutoRun starts now..."
-    local bufnr = vim.fn.input "Bufnr: "
+    print(vim.bo.filetype)
+    local bufnr = create_split_buf(30)
     local command = vim.split(vim.fn.input "Command: ", " ")
+    print(command)
+    print(default_cmd(vim.bo.filetype, "test"))
     local pattern = vim.fn.input "Pattern: "
     attach_to_buffer(tonumber(bufnr), pattern, command)
 end, {})
