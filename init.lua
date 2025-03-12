eb52ba9c5a74336deaa47ba4aeb2b6147f4c5cd2
Xiao Cui <constantine124@gmail.com>
2025-03-11 21:21:38 -0400

    update keymap with updated files

--- init.lua
+++ init.lua
@@ -413,6 +413,7 @@ require("lazy").setup {
       vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
       vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+      vim.keymap.set("n", "<leader>sm", builtin.git_status, { desc = "[S]earch [M]odified files" })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set("n", "<leader>/", function()
